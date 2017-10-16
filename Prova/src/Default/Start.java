48abf242585f745523df1a65af5786289648b5a2
TrustNoMadMan <32836700+TrustNoMadMan@users.noreply.github.com>
2017-10-16 19:01:46 +0200

    Update Start.java

--- Prova/src/Default/Start.java
+++ Prova/src/Default/Start.java
@@ -4,8 +4,9 @@ public class Start {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Ciao");
-		System.out.println("Vuoi delle pere?");
+		while(true){
+			System.out.println("777");
+		}
 	}
 
 }
